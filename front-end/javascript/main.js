d417e49d9b238b893ab9d41388c1ea242a588392
mspcity <codepark2019@gmail.com>
2020-09-13 23:56:05 -0400

    js functions added

--- front-end/javascript/main.js
+++ front-end/javascript/main.js
@@ -1,2 +1,76 @@
-const wordInput = document.querySelector('.word-input')
-console.log(wordInput)
+const GAME_TIME = 3;
+let score = 0;
+let time = GAME_TIME
+let isPlaying = false;
+let timeInterval;
+let checkInterval;
+let words = [];
+
+const wordInput = document.querySelector('.word-input');
+const wordDisplay = document.querySelector('.word-display');
+const scoreDisplay = document.querySelector('.score');
+const timeDisplay = document.querySelector('.time');
+const button = document.querySelector('.button');
+
+init();
+
+function init() {
+  getWords();
+  wordInput.addEventListener('input', checkMatch)
+}
+
+function run() {
+  isPlaying = true;
+  time = GAME_TIME;
+  wordInput.focus();
+  scoreDisplay.innerText = 0;
+  timeInterval = setInterval(countDown, 1000);
+  checkInterval = setInterval(checkStatus, 50)
+  buttonChange('Game playing')
+}
+
+function checkStatus(){
+  if (!isPlaying && time === 0) {
+    buttonChange("start")
+    clearInterval(checkInterval);
+  }
+}
+
+
+function getWords() {
+  words = ['Hello', 'Banana', 'Apple', 'Cherry'];
+  buttonChange('start')
+}
+
+
+function checkMatch () {
+  if (wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()){
+    wordInput.value = "";
+    if (!isPlaying) {
+      return;
+    }
+    score++;
+    scoreDisplay.innerText = score;
+    time = GAME_TIME;
+    const randomIndex = Math.floor(Math.random() * wordInput.length);
+    wordDisplay.innerText = words[randomIndex]
+
+    
+  }
+}
+
+
+function countDown() {
+  time > 0 ? time-- : isPlaying = false;
+  if(!isPlaying) {
+    clearInterval(timeInterval)
+  }
+  timeDisplay.innerText = time;
+}
+
+
+
+function buttonChange(text) {
+  button.innerText = text;
+  text === 'start' ? button.classList.remove('loading') : button.classList.add('loading');
+}
